937f6909db3c45cddcbdd3a18679a87feda7c934
SpeedThreat0308 <119098925+SpeedThreat0308@users.noreply.github.com>
2024-02-21 15:10:08 +0530

    Add files via upload

---
+++ Day 2/user/Home.jsx
@@ -0,0 +1,15 @@
+const HomePage = () => {
+  return (
+    <div>
+      <header>
+        <h1>Welcome To JobXpress</h1>
+      </header>
+      <main>
+        <p></p>
+        <img src="https://via.placeholder.com/300" alt="Placeholder" />
+      </main>
+    </div>
+  );
+}
+
+export default HomePage;
